ed7a1d0c87247a785435a166210299fdc0341167
HPP <harly@prospera>
2026-04-19 20:18:23 -0400

    added a fix for the command control, as well as some preliminary database functionality

--- app/api/auth/sign-in/route.ts
+++ app/api/auth/sign-in/route.ts
@@ -1,5 +1,21 @@
 import { NextRequest, NextResponse } from "next/server";
 
+import {} from "@/lib/supabase/client"
+
+interface SignInParams {
+  username: string;
+  password_hash: string;
+}
+
 export function POST(req: NextRequest){
+  const body = req.body() as SignInParams;
+
+  try{
+    
+  }catch (error){
+    console.error("Error in POST  sign-in route:", error)
+    return NextResponse.json({ message: "Internal server error: Could not sign in" }, { status: 500 } );
+  }
+  
   return NextResponse.json({ message: "Success" }, { status: 200 } );
 }
