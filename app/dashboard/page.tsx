afb8c68fec53976d858e8c48da586c0b0f73d42d
HPP <harly@prospera>
2026-04-20 00:19:55 -0400

    added a mount compoennt

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,29 @@
 'use client'
 import React from "react"
-import { Tldraw } from "tldraw"
+import { Tldraw, Editor, toRichText } from "tldraw"
 import "tldraw/tldraw.css"
 
 function Page(){
+  const handleMount = (editor: Editor) => {
+		editor.createShape({
+			type: 'text',
+			x: 200,
+			y: 200,
+			props: {
+				richText: toRichText('Hello world!'),
+			},
+		})
+
+		editor.selectAll()
+
+		editor.zoomToSelection({
+			animation: { duration: 5000 },
+		})
+	}
+
   return(
     <div className="fixed inset-0">
-      <Tldraw />
+      <Tldraw onMount={handleMount}/>
     </div>
   )
 }
