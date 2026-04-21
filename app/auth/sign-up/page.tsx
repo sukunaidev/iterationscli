5804822b1de937dbda4f6a442e840c9a7825e371
Sukunai <siaaria05@gmail.com>
2026-04-21 03:04:42 -0400

    made signin and signup page

--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -1,12 +1,43 @@
 import React from "react";
+import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
 
 
-function Page(){
+function SignUpPage() {
   return (
-    <>
-      This is a work in progress page
-    </>
+    <div>
+      <div>
+        <div className="flex justify-center mt-90">
+          <Card className="w-full max-w-sm">
+            <div className="flex justify-center">
+              <div className="flex justify-center w-[80px] h-[60px] -mt-4">
+                <img src="/logo--transparent.svg" />
+              </div>
+            </div>
+            <CardHeader className="-mt-5">
+              <CardTitle>Create your Terminal!</CardTitle>
+              <CardDescription>Please carefully enter your information!</CardDescription>
+            </CardHeader>
+            <CardHeader><Separator /></CardHeader>
+            <CardContent>
+              <CardHeader>
+                <CardTitle>Username</CardTitle>
+                <Input placeholder="TankaJahari"></Input>
+                <CardTitle>Password</CardTitle>
+                <Input placeholder="Pizzalover69" type="password"></Input>
+              </CardHeader>
+
+            </CardContent>
+            <Input type="search"></Input>
+            <CardDescription className="text-[10px] -mt-5">Write create terminal then press enter to sign back into your terminal</CardDescription>
+            <Button variant="ghost">{">create terminal cd google"}</Button>
+          </Card>
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default Page;
+export default SignUpPage;
