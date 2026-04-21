c4acda8348e4d2c9a7eedcb810c6077d03e09c58
Sukunai <siaaria05@gmail.com>
2026-04-21 00:01:15 -0400

    imported a card component and made some changes to the sign in page. got rid of sign-in changes that werent commited

--- app/auth/sign-in/page.tsx
+++ app/auth/sign-in/page.tsx
@@ -1,11 +1,22 @@
+import { Input } from "@/components/ui/input";
 import React from "react";
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 
-
-function Page(){
+function Page() {
   return (
-    <>
-      This is a work in progress page
-    </>
+    <div>
+      <div className="flex justify-center">
+
+
+        <Card className="w-full max-w-sm">
+          <CardHeader>
+            <CardTitle>Sign Back Into Your Terminal</CardTitle>
+            <Input placeholder="hello"></Input>
+            <Input placeholder="hello"></Input>
+          </CardHeader>
+        </Card>
+      </div>
+    </div>
   )
 }
 
