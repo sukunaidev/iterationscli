b55b30012cab4a06affd251beed2b5e4f721c18f
Sukunai <siaaria05@gmail.com>
2026-04-21 00:15:21 -0400

    work in progress sign in card

--- app/auth/sign-in/page.tsx
+++ app/auth/sign-in/page.tsx
@@ -1,19 +1,33 @@
 import { Input } from "@/components/ui/input";
 import React from "react";
-import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Label } from "radix-ui";
+import { Separator } from "@/components/ui/separator";
 
 function Page() {
   return (
     <div>
-      <div className="flex justify-center">
+      <div className="flex justify-center mt-90">
 
 
         <Card className="w-full max-w-sm">
           <CardHeader>
             <CardTitle>Sign Back Into Your Terminal</CardTitle>
-            <Input placeholder="hello"></Input>
-            <Input placeholder="hello"></Input>
+            <CardDescription>Please carefully enter your information!</CardDescription>
+
           </CardHeader>
+          <CardHeader><Separator /></CardHeader>
+          <CardContent>
+            <CardHeader>
+              <CardTitle>Username</CardTitle>
+              <Input placeholder="hello"></Input>
+              <CardTitle>Password</CardTitle>
+              <Input placeholder="hello"></Input>
+            </CardHeader>
+
+          </CardContent>
+          <Input></Input>
+          <CardDescription className="text-[10px] -mt-5">Write signin then press enter to sign back into your terminal</CardDescription>
         </Card>
       </div>
     </div>
