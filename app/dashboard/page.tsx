579eaf7b89e1a14ad8cefd45e01916460bd610b5
HPP <harly@prospera>
2026-04-25 04:28:02 -0400

    some testing components for the tldraw library to use kamako boards as one of its tools

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,65 @@
 'use client'
-import React from "react"
-import { Tldraw, Editor, toRichText, TldrawEditorProps } from "tldraw"
-import { KamakoTool } from "@/components/tools/kamako"
-import "tldraw/tldraw.css"
+import {
+	DefaultToolbar,
+	DefaultToolbarContent,
+	Editor,
+	TLComponents,
+	TLUiAssetUrlOverrides,
+	TLUiOverrides,
+	Tldraw,
+	TldrawUiMenuItem,
+	toRichText,
+	useIsToolSelected,
+	useTools,
+} from 'tldraw'
+import 'tldraw/tldraw.css'
+import { ScreenshotTool } from '@/components/tools/screenshot-tool/ScreenshotTool'
+import { KamakoTool } from '@/components/tools/kamako'
+
+// [1]
+const customTools = [ScreenshotTool, KamakoTool]
+
+// [2]
+const customUiOverrides: TLUiOverrides = {
+	tools: (editor, tools) => {
+		return {
+			...tools,
+      kamako: {
+        id: 'kamako',
+        label: "Kamako board",
+        icon: 'tool-kamako',
+        kbd: 'k',
+        onSelect() {
+          editor.setCurrentTool('kamako')
+        }
+      },
+		}
+	},
+}
+
+function CustomToolbar() {
+	const tools = useTools()
+	const isKamakoSelected = useIsToolSelected(tools['kamako'])
+	return (
+		<DefaultToolbar>
+      <TldrawUiMenuItem {...tools['kamako']} isSelected={isKamakoSelected} />
+			<DefaultToolbarContent />
+		</DefaultToolbar>
+	)
+}
+
+const customAssetUrls: TLUiAssetUrlOverrides = {
+	icons: {
+    'tool-kamako': '/kamako-board.svg'
+	},
+}
+
+
+
+const customComponents: TLComponents = {
+	Toolbar: CustomToolbar,
+}
+
 
 function Page() {
 	const handleMount = (editor: Editor) => {
@@ -14,11 +71,19 @@ function Page() {
 				richText: toRichText('Hello world!'),
 			},
 		})
+
 	}
 
 	return (
 		<div className="fixed inset-0">
-			<Tldraw onMount={handleMount} />
+			<Tldraw 
+        persistenceKey="tldraw_screenshot_example"
+        onMount={handleMount}
+        tools={customTools}
+				overrides={customUiOverrides}
+				assetUrls={customAssetUrls}
+				components={customComponents} 
+      />
 		</div>
 	)
 }
--- components/tools/kamako.ts
+++ components/tools/kamako.ts
@@ -1,17 +1,19 @@
-import { StateNode, toRichText } from "tldraw"
+import { StateNode, TLPointerEventInfo, toRichText } from "tldraw"
 
 export class KamakoTool extends StateNode {
-  static override id = "my-tool"
+  static override id = "kamako"
 
   override onEnter = () => {
     console.log("My tool activated")
   }
 
-  override onPointerDown = () => {
+  override onPointerDown = (info: TLPointerEventInfo) => {
+    console.log("info of pointer with kamako:", info);
+
     this.editor.createShape({
       type: "text",
-      x: 200,
-      y: 200,
+      x:  info.point.x - 250,
+      y:  info.point.y - 350,
       props: {
         richText: toRichText("Hello from Kamako"),
       },
