7bbd624a8c0c63112dccd5df19eb9f0cfd882ff8
Sukunai <siaaria05@gmail.com>
2026-05-07 02:31:52 -0400

    added input elements that when filled changes the value of a terminal line

--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -1,11 +1,41 @@
+'use client'
 import React from "react";
+import { useState } from "react";
 import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
+import { useStateTracking } from "tldraw";
 
 
 function SignUpPage() {
+  const [username, setSignUpUsername] = useState("")
+  const [password, setSignUpPassword] = useState("")
+  const [miniTerminalCommand, setMiniTerminalCommand] = useState("")
+  const [googleSignUpButtonClicked, setGoogleSignUpButtonClicked] = useState(false)
+  const text = username != "" && password != "" ? "create terminal" : googleSignUpButtonClicked ? "create terminal via google " : ""
+  const [terminalText, setTerminalText] = useState("")
+  //setTerminalText(text)
+
+
+
+  let handleSignUpUsername = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSignUpUsername(e.target.value);
+  }
+
+  let handleSignUpPassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSignUpPassword(e.target.value);
+  }
+
+  let handleMiniSignUpTerminal = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // setTerminalText(e.target.value)
+    setMiniTerminalCommand(e.target.value);
+    if (miniTerminalCommand == "create terminal") {
+      //take user into their dashboard
+    }
+  }
+
+
   return (
     <div>
       <div>
@@ -24,15 +54,36 @@ function SignUpPage() {
             <CardContent>
               <CardHeader>
                 <CardTitle>Username</CardTitle>
-                <Input placeholder="TankaJahari"></Input>
+                <Input
+                  placeholder="TankaJahari"
+                  value={username}
+                  onChange={handleSignUpUsername}
+                ></Input>
                 <CardTitle>Password</CardTitle>
-                <Input placeholder="Pizzalover69" type="password"></Input>
+                <Input
+                  placeholder="Pizzalover69"
+                  type="password"
+                  value={password}
+                  onChange={handleSignUpPassword}
+                ></Input>
               </CardHeader>
-
             </CardContent>
-            <Input type="search"></Input>
-            <CardDescription className="text-[10px] -mt-5">Write create terminal then press enter to sign back into your terminal</CardDescription>
-            <Button variant="ghost">{">create terminal cd google"}</Button>
+            <Input
+
+              value={text}
+              /* This means that if the username and password are not empty
+                  then the terminal will have the value of create terminal
+                  if the google sign up button is clicked then create teminal via google
+                  else the string will be empty
+              */
+              onChange={handleMiniSignUpTerminal}
+            ></Input>
+            <CardDescription className="text-[10px] -mt-5">Write create terminal then press enter to sign into your terminal</CardDescription>
+            <Button variant="ghost"
+              onClick={() => {
+                setGoogleSignUpButtonClicked(true)
+              }}
+            >{">create terminal via google"}</Button>
           </Card>
         </div>
       </div>
