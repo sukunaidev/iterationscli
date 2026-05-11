89700eebc9663e36e62a4041ad5b1739738615d2
HPP <harly@prospera>
2026-05-11 03:24:14 -0400

    added some comments to explain the context

--- contexts/AuthenticateContext.tsx
+++ contexts/AuthenticateContext.tsx
@@ -22,13 +22,19 @@ export function AuthenticateProvider({ children }: AuthenticateProviderProps) {
     expires_at: null,
   })
     
+  // the check session function is the behavior that can be used globally across the application, to verify if a user is authenticated whenever needed. 
+  // In addition it refreshes user object data with the latest data, otherwise it will clear the USER object and set any error it runs into.
   const checkSession = async () => {
     try{
+      // using the authClient class we call the GetUser function to get user data
       const { expires_at, user, error } = await authClient.GetUser();
+
+      // if we do NOT receive a user object and get an ERROR object, we must clear the authentication context. (something in the auth process/serverside has failed)
       if(!user && error){
         // we can more specifically set the auth state, based on the error (if the cookie expired, etc.)
         console.warn("Error: User is not authenticated: ", error)
-
+        
+        // now we clear the user object, and setting a GLOBAL AUTH ERROR so the rest of the applicaiton is aware
         setAuth((prev) => ({ 
           ...prev,
           is_loading: false,
@@ -38,6 +44,8 @@ export function AuthenticateProvider({ children }: AuthenticateProviderProps) {
         }));
 
       }
+
+      // otherwise if we recieve both a USER OBJECT and the timestamp that the USER OBJECT WILL EXPIRE AT, we must set them inside of the context
       if(user && expires_at){
 
         setAuth((prev) => ({
@@ -49,6 +57,7 @@ export function AuthenticateProvider({ children }: AuthenticateProviderProps) {
         }));
 
       } else{
+        // if we DONT get a USER or the EXPIRAY TIMESTAMP, then we know yet again something went wrong in the auth process, set an error state.
         console.error("User: Something went wrong authenticating user: User or expires_at was null", { user, expires_at })
 
         setAuth((prev) => ({ 
@@ -60,6 +69,7 @@ export function AuthenticateProvider({ children }: AuthenticateProviderProps) {
         }));
       }
 
+      // Again something may have failed in the authClient.GetUser function, maybe the user's internet went out, or the server is down (HTTP status code 500).
     } catch(error) {
       console.error("Failed to authenticate user: ", error)
 
