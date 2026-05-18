f36003d94e4cf69f47e3af5b5fa2c16d75652b7f
Sukunai <siaaria05@gmail.com>
2026-05-18 19:43:27 -0400

    updating code, getting error eith deployment. maybe pushing changes will help after a merge

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,22 +1,12 @@
 'use client'
 import UserSettingsPage from "@/components/auth/UserSettings";
 import KamakoBoard from "@/components/shapes/KamakoBoard";
-import DocumentDescription from "@/components/shapes/DocumentDescription";
-import {
-  ResizableHandle,
-  ResizablePanel,
-  ResizablePanelGroup,
-} from "@/components/ui/resizable"
+
 function Page() {
   return (
     <div>
       <KamakoBoard />
     </div>
-
-
-
-
-
 
   )
 }
